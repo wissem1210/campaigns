a3af998683f3294455690bc499f41241110b7804
wissemMaalel <maalelwissem@gmail.com>
2020-01-20 12:20:27 +0100

    campaign content

--- src/data/repositories/compaignRepository.js
+++ src/data/repositories/compaignRepository.js
@@ -2,222 +2,146 @@ import { BaseRepository } from "./base/repository"
 
 export class CompaignRepository extends BaseRepository {
     constructor() {
-        super([
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
+        super([{
+                img: "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
                 name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
+                duration: "14 days",
                 status: "Published"
             },
             {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
+                img: "https://cdn.pixabay.com/photo/2020/01/14/06/52/auto-garage-4764243_960_720.png",
                 name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
+                duration: "17 days",
                 status: "Processing"
             },
             {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
+                img: "https://cdn.pixabay.com/photo/2016/03/09/15/22/painter-1246619_960_720.jpg",
+                name: "billborads",
+                duration: "30 days",
                 status: "Published"
             },
             {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
+                img: "https://cdn.pixabay.com/photo/2015/12/07/10/59/building-1080594_960_720.jpg",
+                name: "Focus",
+                duration: "31 days",
                 status: "Processing"
             },
             {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
+                img: "https://cdn.pixabay.com/photo/2016/10/09/16/57/sale-1726232_960_720.jpg",
+                name: "Orchard",
+                duration: "21 days",
                 status: "Published"
             },
             {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
+                img: "https://cdn.pixabay.com/photo/2017/10/27/16/34/cavitacion-2894835_960_720.jpg",
+                name: "rendez vous",
+                duration: "14 days",
                 status: "Processing"
             }, {
                 img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
+                name: "Singapore",
+                duration: "11 days",
                 status: "Published"
             },
             {
                 img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
+                name: "Nes",
+                duration: "26 days",
                 status: "Processing"
             },
             {
+                img: "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
+                name: "UA Sports Singapore",
+                duration: "14 days",
+                status: "Published"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2020/01/14/06/52/auto-garage-4764243_960_720.png",
+                name: "Nespresso",
+                duration: "17 days",
+                status: "Processing"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2016/03/09/15/22/painter-1246619_960_720.jpg",
+                name: "billborads",
+                duration: "30 days",
+                status: "Published"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2015/12/07/10/59/building-1080594_960_720.jpg",
+                name: "Focus",
+                duration: "13 days",
+                status: "Processing"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2016/10/09/16/57/sale-1726232_960_720.jpg",
+                name: "Orchard",
+                duration: "21 days",
+                status: "Published"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2017/10/27/16/34/cavitacion-2894835_960_720.jpg",
+                name: "rendez vous",
+                duration: "14 days",
+                status: "Processing"
+            }, {
                 img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
+                name: "Singapore",
+                duration: "11 days",
                 status: "Published"
             },
             {
                 img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
+                name: "Nes",
+                duration: "26 days",
+                status: "Processing"
+            }, {
+                img: "https://cdn.pixabay.com/photo/2016/04/01/12/16/car-1300629_960_720.png",
+                name: "UA Sports Singapore",
+                duration: "14 days",
+                status: "Published"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2020/01/14/06/52/auto-garage-4764243_960_720.png",
                 name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
+                duration: "17 days",
                 status: "Processing"
             },
             {
+                img: "https://cdn.pixabay.com/photo/2016/03/09/15/22/painter-1246619_960_720.jpg",
+                name: "billborads",
+                duration: "30 days",
+                status: "Published"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2015/12/07/10/59/building-1080594_960_720.jpg",
+                name: "Focus",
+                duration: "31 days",
+                status: "Processing"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2016/10/09/16/57/sale-1726232_960_720.jpg",
+                name: "Orchard",
+                duration: "21 days",
+                status: "Published"
+            },
+            {
+                img: "https://cdn.pixabay.com/photo/2017/10/27/16/34/cavitacion-2894835_960_720.jpg",
+                name: "rendez vous",
+                duration: "14 days",
+                status: "Processing"
+            }, {
                 img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
+                name: "Singapore",
+                duration: "11 days",
                 status: "Published"
             },
             {
                 img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
+                name: "Nes",
+                duration: "26 days",
                 status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
-            {
-                img: "https://underarmour.scene7.com/is/image/Underarmour/SS20_Run_HOVR_Sonic3_BKT?scl=1",
-                name: "UA Sports Singapore",
-                duration: "01 Apr - 31 Apr 2019",
-                status: "Published"
-            },
-            {
-                img: "https://www.nespresso.com/ecom/medias/sys_master/public/12839343456286/Nespresso-OG-1080x1080v2.jpg",
-                name: "Nespresso",
-                duration: "26 May - 20 Apr 2019",
-                status: "Processing"
-            },
+            }
         ])
     }
 }
